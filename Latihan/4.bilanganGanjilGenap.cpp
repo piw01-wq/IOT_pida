6d1e3856fb5a7b0c15e53b5644e21ea5fe887bf2
piw01-wq <nur.hafidh44@gmail.com>
2025-08-14 14:00:01 +0700

    Update 4.bilanganGanjilGenap.cpp

--- Latihan/4.bilanganGanjilGenap.cpp
+++ Latihan/4.bilanganGanjilGenap.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int angka;
+
+  cout << "=== Program Menentukan Bilangan Ganjil/Genap ===" << endl;
+  cout << "Masukkan sebuah bilangan: ";
+  cin >> angka;
+
+  if (angka % 2 == 0) {
+    cout << angka << "adalah bilangan GENAP." << endl;
+  } else {
+    cout << angka << "adalah bilangan GANJIL." << endl;
+  }
+
+ return 0;
+}
 
