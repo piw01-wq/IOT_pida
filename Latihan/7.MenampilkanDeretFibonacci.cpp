2504f9e4c39cef1aec3a00921f5748ab4ca7b9e8
piw01-wq <nur.hafidh44@gmail.com>
2025-08-15 12:57:34 +0700

    Update 7.MenampilkanDeretFibonacci.cpp

--- Latihan/7.MenampilkanDeretFibonacci.cpp
+++ Latihan/7.MenampilkanDeretFibonacci.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
 using namespace std;
+void fibonacci(int n) {
+  int first = 0, srcond = 1, next;
+  cout << "Deret Fibonacci (" << n << " suku): ";
+  for (int i = 0; i < n; i++) {
+    if (i <= 1)
+      next = i;
+    else {
+      next = first + second;
+      first = second;
+      second = next;
+    }
+    cout << next << " ";
+  }
+}
+int main() {
+  int n;
+  cout << "Masukkan jumlah suku fibonacci; ";
+  cin >> n;
+
+  if (n <= 0)
+    cout << "Masukkan bilangan positif!";
+else
+    fibonacci(n);
+  return 0;
+}
