d9145f85352dc19e6be3248456f01687dff37a1a
piw01-wq <nur.hafidh44@gmail.com>
2025-08-15 13:45:46 +0700

    Update 8.KalkulatorSederhana.cpp

--- Latihan/8.KalkulatorSederhana.cpp
+++ Latihan/8.KalkulatorSederhana.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char op;
+  double num1,num2;
+
+  cout << "Masukkan operator (+, -, *, /); ";
+  cin >> op;
+
+  cout << "Masukkan bilangan: ";
+  cin >> num1 >> num2;
+
+  switch(op) {
+case '+':
+  cout << num1 << " + " << num2 << " = " << num1 + num2;
+  break;
+case '-':
+  cout << num1 << " - " << num2 << " = " << num1 - num2;
+  break;
+case '*':
+  cout << num1 << " * " << num2 << " = " << num1 * num2;
+  break;
+case '/':
+  if (num2 != 0)
+    cout << num1 << " / " << num2 << " = " << num1 / num2;
+else
+    cout << "Error! Pembagian dengan nol tidak diperbolehkan.";
+break;
+default:
+  cout << "Operator tidak valid!";
+  }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
 
