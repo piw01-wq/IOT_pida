86f1ff80a6ccaca8f1aa8e2704425fefa4b29d09
piw01-wq <nur.hafidh44@gmail.com>
2025-08-15 12:31:36 +0700

    Update 6.TahunKabisat.cpp

--- Latihan/6.TahunKabisat.cpp
+++ Latihan/6.TahunKabisat.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+bool isLeapYear(int year) {
+  //Tahun kabisat jika:
+  //1. HAbis dibagi 4 tetapi tidak habis dibagi 100,ATAu
+  //2. Habis dibagi 400
+  return (year % 4 == 0 && year % 100 !=0) || (year % 400 == 0);
+}
+int main() {
+  int year;
+  cout << "masukkan tahun: ";
+  cin >> year;
+      if (isLeapYear(year)} {
+      cout << year << "adalah tahun kabisat." << endl;
+} else {
+      cout << year << " bukan tahun kabisat." << endl;
+}
+      return 0;
+}
