4eb982a1c891a298c24f46cc8305fef1696fdd30
piw01-wq <nur.hafidh44@gmail.com>
2025-08-22 12:24:55 +0700

    Update 10.mencariNilaiTerbesar.cpp

--- Latihan/10.mencariNilaiTerbesar.cpp
+++ Latihan/10.mencariNilaiTerbesar.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+  double a, b, c;
+
+  cout << "PROGRAM MENCARI NILAI TERBESAR DARI 3 ANGKA" << endl;
+  cout << "===========================================" << endl;
+
+  // Input tiga angka
+  cout << "Masukkan angka pertama: ";
+  cin >> a;
+  cout << "Masukkan angka kedua: ";
+  cin >> b;
+  cout << "Masukkan angka ketiga: ";
+  cin >> c;
+
+  // Mencari nilai terbesar
+  if (a >= b && a >= c) {
+     cout << "Nilai terbesar adalah: " << a << endl;
+  } else if (b >= a && b >= c) {
+     cout << "Nilai terbesar adalah: " << b << endl;
+  } else {
+     cout << "Nilai terbesar adalah: " << c << endl;
+  }
+
+return 0;
+}
