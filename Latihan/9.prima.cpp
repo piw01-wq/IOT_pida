a2801c2e62249aa2e5597940498d12b3c29572c0
piw01-wq <nur.hafidh44@gmail.com>
2025-08-22 10:35:53 +0700

    Update 9.prima.cpp

--- Latihan/9.prima.cpp
+++ Latihan/9.prima.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
+#include <cmath> // untuk fungsi sqrt ()
 using namespace std;
+bool isPrime(int n) {
+if (n <= 1) {
+    return false; //bilangan <= 1 bukan prima
+}
+if (n == 2)
+    return true; //2 adalah bilangan prima
+}
+if (n % 2 == 0) {
+   return false; //bilangan genap selain 2 bukan prima
+}
+   //cek pembagi dari 3 hingga akar kuadrat n
+for (int i = 3; i <= sqrt(n); i +=2) {
+    if (n % i == 0) {
+       return false; // ditemukan pembagi
+    }
+ }
+ return true; //bilangan prima
+}
+
+int main() {
+  int number;
+
+cout << "Masukkan sebuah bilangan: ";
+cin >> number;
+
+if (isPrime(number)) {
+   cout << number << " adalah bilangan PRIMA" << endl;
+} else {
+   cout << number << " adalah BUKAN bilangan prima" << endl;
+}
+
+return 0;
+}
+
 
