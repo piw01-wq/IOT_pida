1dd8ef79a5a746478a56e78c8ea7971ca1902a8c
piw01-wq <nur.hafidh44@gmail.com>
2025-09-19 12:41:05 +0700

    Update 17.Mengecek_palindrom.cpp

--- Latihan/17.Mengecek_palindrom.cpp
+++ Latihan/17.Mengecek_palindrom.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
 
+int main() {
+  string teks, asli;
+  cout << "Masukkan sebuah kata/kalimat: ";
+  getline (cin, teks);
+  asli = teks;
+
+  teks.erase(remove(teks.begin(), teks.end()), teks.end());
+  transform(teks.begin(), teks.end(), teks.begin(), ::tolower);
+  string terbalik = teks;
+  reverse(terbalik.begin(), terbalik.end());
+
+  if (teks == terbalik) {
+      cout << "\"" << asli << "\" adalah palindrome." << endl;
+  } else {
+      cout << "\"" << asli << "\" bukan palindrom." << endl;
+  }
+  return 0;
+}
+      
