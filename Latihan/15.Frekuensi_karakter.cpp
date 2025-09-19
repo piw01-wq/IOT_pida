94bc47157ba428fa809cf5e14a1ee54505b01dc0
piw01-wq <nur.hafidh44@gmail.com>
2025-09-19 10:36:13 +0700

    Update 15.Frekuensi_karakter.cpp

--- Latihan/15.Frekuensi_karakter.cpp
+++ Latihan/15.Frekuensi_karakter.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+  string teks = "programming";
+  map<char,int> frekuensi;
+
+  for (char c : teks) {
+      frekuensi[c]++;
+  }
+
+  cout << "Frekuensi karakter:" <<endl;
+  for (auto pair : frekuensi) {
+      cout << "'" << pair.first << "': " << pair.second << endl;
+  }
+  return 0;
+}
