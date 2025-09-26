6b3f3d286b96c723de7ce3d4dc8d619ebbe87b9c
piw01-wq <nur.hafidh44@gmail.com>
2025-09-26 11:41:15 +0700

    Update 21.Menghitung_pangkat.cpp

--- Latihan/21.Menghitung_pangkat.cpp
+++ Latihan/21.Menghitung_pangkat.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int hitungPangkat(int basis, int eksponen) {
+  int hasil = 1;
+  for (int i = 0; i < eksponen; i++) {
+      hasil *= basis;
+  }
+  return hasil;
+}
+
+int main() {
+  int a, b;
+  cout << "Masukkan bilangan basis: ";
+  cin >> a;
+  cout << "Masukkan bilangan pangkat: ";
+  cin >> b;
+  cout << a << " ^ " << b << " = " << hitungPangkat(a, b) << endl;
+
+return 0;
+}
