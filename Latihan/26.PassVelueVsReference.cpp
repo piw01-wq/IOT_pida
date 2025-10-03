07c72cb78649494c8fc2acd00ca4542f65489e0e
piw01-wq <nur.hafidh44@gmail.com>
2025-10-03 14:05:11 +0800

    Update 26.PassVelueVsReference.cpp

--- Latihan/26.PassVelueVsReference.cpp
+++ Latihan/26.PassVelueVsReference.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
-void ubahValue(int aa) {
+void ubahValue(int a) {
   a = a + 10;
   cout << "Nilai di dalam fungsi (pass by value) : " << a << endl;
 }
-void ubahReference(int &ba) {
+void ubahReference(int &b) {
   b = b + 10;
   cout << "Nilai di dalam fungsi (pass by reference) : " << b << endl;
 }
@@ -12,7 +12,7 @@ void ubahReference(int &ba) {
 int main() {
   int x = 5;
   int y = 5;
-  cout << "Sebelum fungsi di panggil:" endl;
+  cout << "Sebelum fungsi di panggil:" <<  endl;
   cout << "x = " << x << " (untuk pass by value)" << endl;
   cout << "x = " << x << " (untuk pass by value)" << endl;
   cout << "y = " << y << " (untuk pass by reference)" << endl;
